c1f03d0720bbdf5d23f33bdd65a76832f6872f54
cam-tran <cam.tran@sfr.fr>
2022-03-30 13:20:41 +0100

    add

--- js/index.js
+++ js/index.js
@@ -12,4 +12,16 @@ function lorsDuChargementDeLaPage(){
 // losrque DOMContentLoaded est chargé on appelle la fonction lorsDuChargementDeLaPage
 document.addEventListener('DOMContentLoaded',lorsDuChargementDeLaPage);
 
+function setMemeOnSVGViewer(meme){
+    //verifier le parametre n'est pas undefine
+    //return;  pour sortir de function
+    if(undefined === meme){ 
+        meme= new Meme();
+        meme.setDummyValues();
+    }
+    var svgTextNode =document.querySelector('svg text');
+    //modif du contenu html de la balise
+    svgTextNode.innerHTML= meme.text;
 
+
+}
--- js/meme-es5.js
+++ js/meme-es5.js
@@ -3,8 +3,10 @@
  */
 function Meme(){
     // champs public sur l'instance de this
+    
+    this.id = undefined;
     this.name="Le nom";
-    this.texte="le text";
+    this.texte="text";
     this.x=0;
     this.y=0;
     this.fontSize=12;
@@ -13,8 +15,23 @@ function Meme(){
     this.italic=false;
     this.color='#000000';
     this.image ='image1.jpg';
-    console.log(this);
+    console.log('for meme construite',this);
+
+    this.setDummyValues = function createDummyMeme(params){
+        this.name="Dummy demo meme";
+        this.texte="mon texte";
+        this.x=30;
+        this.y=50;
+        this.fontSize=20;
+        this.fontWeight ='900';
+        this.underline=true;
+        this.italic=false;
+        this.color='#ACACAC';
+        this.image ='image1.jpg';
+    }
 }
 
 //object dont la forme ne peut pas changer
 var unMemeGlobal= Object.seal(new Meme());
+unMemeGlobal.setDummyValues();
+

91bfb36b091f09fbcfe7a2ae772f3c1a3a7c2b92
cam-tran <cam.tran@sfr.fr>
2022-03-30 09:18:02 +0100

    modif style, ajout ;

--- js/index.js
+++ js/index.js
@@ -3,4 +3,4 @@ var baliseisJsLoaded = document.querySelector("#isJsLoaded");
 console.log('JS est chargé', baliseisJsLoaded );
 baliseisJsLoaded.innerHTML = 'JS chargé';
 baliseisJsLoaded.style.backgroundColor='skyblue';
-baliseisJsLoaded.style.fontSize='30px'
+baliseisJsLoaded.style.fontSize='30px';

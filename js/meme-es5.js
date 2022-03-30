4c521a8f0106ea78925bed99e6dac42329865c2a
cam-tran <cam.tran@sfr.fr>
2022-03-30 11:08:21 +0100

    MAJ

--- js/meme-es5.js
+++ js/meme-es5.js
@@ -5,26 +5,14 @@ function Meme(){
     // champs public sur l'instance de this
     this.name="Le nom";
     this.texte="le text";
-    // fonction privé local a l'objet
-    function _unePrivateFn() {
-        console.log('hello');
-    }
-    // champs privé local(interne) à l'objet
-    var _unePrivateVar="private value"
-    //exposition public d'un fonction
-    this.maPublicFn=_unePrivateFn;
-    //fonction public
-    this.fn=function (uneValue) {
-        //acces au parametre d'entrée de la fonction fn
-        console.log(uneValue);
-        // acces au champs public de l'instance
-        console.log(this.name);
-        // acces a la fonction public de l'instance
-        this.maPublicFn();
-        // acces au champs privé de l'instance
-        console.log(_unePrivateVar);
-        // execution d'une fonction privé de l'instance
-        _unePrivateFn();
-    }
+    this.x=0;
+    this.y=0;
+    this.fontSize=12;
+    this.fontWeight ='900';
+    this.underline=false;
+    this.italic=false;
+    this.color='#000000';
+    this.image ='image1.jpg';
+    console.log(this);
 }
 var unMemeGlobal=new Meme();

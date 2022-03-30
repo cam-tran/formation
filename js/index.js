747c6267bcb90428246d687670511fd4cfb3a534
cam-tran <cam.tran@sfr.fr>
2022-03-30 09:28:08 +0100

    isNotLoaded to isLoaded

--- js/index.js
+++ js/index.js
@@ -2,5 +2,7 @@
 var baliseisJsLoaded = document.querySelector("#isJsLoaded");
 console.log('JS est chargé', baliseisJsLoaded );
 baliseisJsLoaded.innerHTML = 'JS chargé';
-baliseisJsLoaded.style.backgroundColor='skyblue';
-baliseisJsLoaded.style.fontSize='30px';
+//baliseisJsLoaded.style.backgroundColor='skyblue';
+//baliseisJsLoaded.style.fontSize='30px';
+
+baliseisJsLoaded.className='isLoaded';

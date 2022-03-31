440608d58812311e69be67637acd23f3df68dd32
cam-tran <cam.tran@sfr.fr>
2022-03-31 13:18:15 +0100

    callback

--- js/CRUD.js
+++ js/CRUD.js
@@ -1,10 +1,16 @@
-/**
- * callback est un nom variable pour dire que c'est une fonction callback
- * les derniers variables sont facultatifs
- * @param {*} RestAdr 
- */
+ /**
+     * Fonction generique pour appel XHR
+     * @param {string} ressourceUrl commence par /
+     * @param {Function} successCallback conction de traitement du resultat success 
+     * @param {string} method method HTTP pour l'appel  def. GET
+     * @param {number} id surcharge de l'id de la ressource pour la creation de l'url 
+     * @param {(object|Array|string|number} body contenu objet, array, number, ou string du contenu (les object/array serons traités par JSON.stringify) 
+     * @param {string} contentType contenu du header Content-Type
+     * @param {Function} unsuccessCallback fonction de traitement du resultat non valid unsuccess 
+     * 
+     */
 function CRUD(RestAdr){
-    this.callXHR = function(ressourceUrl,callback,method,id,body,contentType){
+    this.callXHR = function(ressourceUrl,succesCallback,method,id,body,contentType,unsuccesCallback){
         if(undefined === ressourceUrl){
             console.log('%c%s', 'color:red;font-weight:900;font-size:20pt','callXHR resoource not given !!! ');
             return;
@@ -27,7 +33,17 @@ function CRUD(RestAdr){
         //gestion de event
         xhr.onreadystatechange = function(evt){
             if(xhr.readyState < XMLHttpRequest.DONE){return};
-            if(xhr.status >= 400){return};
+
+            if(xhr.status >= 400){
+                if(undefined !== unsuccesCallback && typeof(unsuccesCallback) ==='function'){
+                    unsuccesCallback(xhr);
+                }
+                return};
+
+            //exec de la fonction sucess
+            if(undefined !== succesCallback && typeof(succesCallback) ==='function'){
+                succesCallback(xhr.response);
+            }
             console.log(xhr.response);
             console.log(evt);
         }
@@ -45,4 +61,4 @@ function CRUD(RestAdr){
 var xhr = new CRUD('http://localhost:5679');
 xhr.callXHR('/images',function(){});  //GET par defaut
 
-xhr.callXHR('/images',function(){},'POST', undefined,{ch1:'hello'},'application/json');
+//xhr.callXHR('/images',function(){},'POST', undefined,{ch1:'hello'},'application/json');
